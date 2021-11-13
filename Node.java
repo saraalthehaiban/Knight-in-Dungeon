62b557792b8e2fc6e266812a03d05ba21919bf9a
samarff <samar-alq@hotmail.com>
2021-11-13 14:52:04 +0300

    node setup

--- Node.java
+++ Node.java
@@ -1,5 +1,41 @@
 public class Node {
-    public static void main(String[] args) {
-        System.out.println("hi,mi name is samar");
+
+        int x; //x cordnaite
+        int y; //y cordinate
+        boolean obctical; 
+        boolean visited; 
+    
+        Node(int x, int y, boolean obctical) {
+            this.x = x;
+            this.y = y;
+            this.obctical = obctical;
+            visited = false;
+        }
+   
+ 
+
+        void setVisit() {
+            visited = true;
+        }
+    
+        void setUnvisit() {
+            visited = false;
+        }
+        void removeObctical(){
+            obctical=false;
+        }
+
+        //setters and getters
+        int getX(){
+            return x;
+        }
+        int getY(){
+            return y;
+        }
+        boolean getObctical(){
+            return obctical;
+        }
+        boolean getVisited(){
+        return visited;
+        }
     }
-}
