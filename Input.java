994ff7192123a349a4adbd8dec18a3d7f161f195
Rasha AlSughier <rasha.alsughier@gmail.com>
2021-11-29 14:42:59 +0300

    fixed the scanner warning

--- Input.java
+++ Input.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 public class Input{
+    @SuppressWarnings("resource")
     public static void main(String[] args) {
        
        Scanner sc = new Scanner(System.in); //Scanner declaration to read from the user
